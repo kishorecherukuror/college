34bf7c27da93fed426985d53f651b751938aea84
kishorecherukuror <kishorecheruku.ror@gmail.com>
2017-05-15 09:20:31 +0530

    crud

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,12 +4,7 @@ class EmployeesController < ApplicationController
   end
 
   def create
-  	
-  	@employee = Employee.new(name: params[:employee][:name],
-  		age: params[:employee][:age],
-  		salary: params[:employee][:salary],
-  		status: params[:employee][:status])
-
+  	@employee = Employee.new(employees_params)
   	if @employee.save
   		redirect_to employees_index_path
   	end
@@ -17,5 +12,33 @@ class EmployeesController < ApplicationController
 
   def index
   	@employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find params[:id]
+  end
+
+  def destroy
+    @employee = Employee.find params[:id]
+    if @employee.destroy
+      redirect_to "/"
+    end
+  end
+
+  def edit
+    @employee  = Employee.find params[:id]
+  end
+
+  def update
+    @employee  = Employee.find params[:id]
+    if @employee.update_attributes(employees_params)
+      redirect_to "/"
+    end
+  end
+
+
+ private
+ 	def employees_params
+ 		params.require(:employee).permit!
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root "employees#index"
   get 'employees/new'
+  get 'employees/:id/show', to: "employees#show", as: "kishore"
 
+  delete 'employees/:id/destroy', to: "employees#destroy", as: "destroy"
+
+  get 'employees/:id/edit', to: "employees#edit", as: "edit"
+  patch 'employees/update'
   get 'employees/create'
   post 'employees/create'
 
